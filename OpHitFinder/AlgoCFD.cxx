ff7f60265d7c4192f95ff9410ad30f471a20f00e
Victor Genty <vgenty@nevis.columbia.edu>
2015-11-08 19:30:46 -0600

    Let the found pulse fall a bit more before calculating area

--- OpHitFinder/AlgoCFD.cxx
+++ OpHitFinder/AlgoCFD.cxx
@@ -28,14 +28,6 @@ namespace pmtana{
 
     _F = pset.get<float>("Fraction");
     _D = pset.get<int>  ("Delay");
-    
-    
-    // _adc_thres = pset.get<float>("ADCThreshold");
-    // _end_adc_thres = pset.get<float>("EndADCThreshold");
-    // _nsigma = pset.get<float>("NSigmaThreshold");
-    // _end_nsigma = pset.get<float>("EndNSigmaThreshold");
-    // _verbose = pset.get<bool>("Verbosity");
-    // _num_presample = pset.get<size_t>("NumPreSample");
 
     Reset();
 
@@ -70,8 +62,8 @@ namespace pmtana{
 
     std::vector<double> cfd; cfd.reserve(wf.size());
 
-    /// we are going to lose D values of of each end since we go left to right
-    /// later we can go the other direction
+    // follow cfd procedure: invert waveform, multiply by constant fraction
+    // add to delayed waveform.
     for (unsigned k = 0; k < wf.size(); ++k)  {
       
       auto delayed = -1.0 * _F *  ( (float) wf.at(k) - mean_v.at(k) );
@@ -86,36 +78,42 @@ namespace pmtana{
     }
 
 
-    //Get the zero point crossings, how can I tell which are meaningful?
-    //go to each crossing, see if waveform is above pedestal (high above pedestal
+    // Get the zero point crossings, how can I tell which are meaningful?
+    // go to each crossing, see if waveform is above pedestal (high above pedestal)
+
     auto crossings = LinearZeroPointX(cfd);
-    auto threshold = 5.0;
     auto number_presample = 5;
+
+    auto peak_thresh  = 5.0;
+    auto start_thresh = 5.0;
+    auto end_thresh   = 1.5;
+    
     double before_mean, after_mean;
     before_mean = after_mean = 0;
 
-    auto in_peak = [&wf,&sigma_v,&mean_v,&threshold](int i) -> bool
-      { return wf.at(i) > sigma_v.at(i) * threshold + mean_v.at(i); };
+    // lambda criteria to determine if inside pulse
     
+    auto in_peak = [&wf,&sigma_v,&mean_v](int i, float thresh) -> bool
+      { return wf.at(i) > sigma_v.at(i) * thresh +  mean_v.at(i); };
+
+    // loop over CFD crossings
     for(const auto& cross : crossings) {
 
-      if( wf.at( cross.first ) > sigma_v.at( cross.first ) * threshold +  mean_v.at( cross.first ) ) {
-	
+      //      if( wf.at( cross.first ) > sigma_v.at( cross.first ) * threshold +  mean_v.at( cross.first ) ) {
+      if( in_peak( cross.first, peak_thresh) ) {
 	_pulse.reset_param();
 
 	int i = cross.first;
 
 	//backwards
-	//while ( wf.at(i) > sigma_v.at(i) * threshold + mean_v.at(i) ) {
-	while (in_peak(i) ){
+	while ( in_peak(i, start_thresh) ){
 	  i--;
 	  if ( i < 0 ) { i = 0; break; }
 	}
 	_pulse.t_start = i;
 	
 	//walk a little further backwards to see if we can get 5 low RMS
-	//while ( wf.at(i) < sigma_v.at(i) * threshold + mean_v.at(i) ) {
-	while ( !in_peak(i) ) {
+	while ( !in_peak(i,start_thresh) ) {
 	  if (i == ( _pulse.t_start - number_presample ) ) break;
 	  i--;
 	  if ( i < 0 ) { i = 0; break; }
@@ -127,8 +125,7 @@ namespace pmtana{
 	i = _pulse.t_start + 1;
 	
 	//forwards
-	//while ( wf.at(i) > sigma_v.at(i) * threshold + mean_v.at(i) ) {
-	while ( in_peak(i) ) {
+	while ( in_peak(i,end_thresh) ) {
 	  i++;
 	  if ( i > wf.size() - 1 ) { i = wf.size() - 1; break; }
 	}
@@ -136,8 +133,7 @@ namespace pmtana{
 	_pulse.t_end = i;
 
 	//walk a little further forwards to see if we can get 5 low RMS
-	//while ( wf.at(i) < sigma_v.at(i) * threshold + mean_v.at(i) ) {
-	while ( !in_peak(i) ) {
+	while ( !in_peak(i,end_thresh) ) {
 	  if (i == ( _pulse.t_end + number_presample ) ) break;
 	  i++;
 	  if ( i > wf.size() - 1 ) { i = wf.size() - 1; break; }
@@ -158,8 +154,10 @@ namespace pmtana{
 	_pulse.peak       = *it - _pulse.ped_mean;
 	_pulse.t_cdfcross =  cross.second;
 
-	for(auto k = _pulse.t_start; k <= _pulse.t_end; ++k)
-	  _pulse.area += wf.at(i) - _pulse.ped_mean;
+	for(auto k = _pulse.t_start; k <= _pulse.t_end; ++k) {
+	  auto a = wf.at(i) - _pulse.ped_mean;
+	  if ( a > 0 ) _pulse.area += a
+	}
 	  
 	_pulse_v.push_back(_pulse);
       }
